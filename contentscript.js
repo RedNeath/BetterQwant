1ba5b5f960cc145cabf2d1475ae09a7e9c101252
D-Ar-Heten <74800429+D-Ar-Heten@users.noreply.github.com>
2022-06-09 03:53:21 +0200

    accueil

--- contentscript.js
+++ contentscript.js
@@ -13,13 +13,18 @@ for (i = 0; i < children.length; i++) {
 }
 style = document.createElement("link");
 style.rel = "stylesheet";
-style.href = "https://cdn.jsdelivr.net/gh/RedNeath/BetterQwant/colors.css";
+style.href = "https://cdn.discordapp.com/attachments/485070483260440581/984272042054545418/colors.css"//"https://cdn.jsdelivr.net/gh/RedNeath/BetterQwant/colors.css";
 document.head.removeChild(child);
 document.head.insertBefore(style, children[i]);
 
 
-//We delete the main picture (because it has an integrated background color)
-var elems = document.getElementsByClassName('Hero-module__HeroPart___3YcuN Hero-module__HeroPartImage___sxOXJ Hero-module__HeroImageBottom___33H2i Flex-module__flex___35zId Flex-module__column___1m4v1 Flex-module__center___2VIUB Flex-module__alignCenter___24fCk');
+//We delete the first div because we don't need an ad for a site we're already on
+var elems = document.getElementsByClassName('Hero-module__HeroContainer___3ZALZ Box-module__relative___1cR1n');
 for (let i = 0; i < elems.length; i++) {
 	elems[i].parentNode.removeChild(elems[i]);
+}
+
+elems = document.getElementsByClassName('Section-module__SectionContainer___1EWww');
+for (let i = 0; i < elems.length; i++) {
+	elems[i].style = "";
 }
